d186326dcfc7a4497a13262f6842911c9dc74c0c
Josh Deaton <jdeaton@users.noreply.github.com>
2019-06-14 07:03:24 -0400

    Update pretty_output for correct # of nodes.

--- WFEM/pretty_output.m
+++ WFEM/pretty_output.m
@@ -1,6 +1,6 @@
 function [] = pretty_output(X)
-
-n = length(X)/6;
+global nodes
+[n, ~] = size(nodes);
 fprintf('Node_# trans_x     trans_y     trans_z     rot_x       rot_y        rot_z\n')
 for i = 1:n
     d = full(X(6*i-5:6*i));
