e8c8dc1d091e7bd43f8f0ed8f9bca37b20e442e5
Josh Deaton <jdeaton@users.noreply.github.com>
2019-06-13 22:45:53 -0400

    Static load example. Added function for pretty printing displacements.

---
+++ WFEM/pretty_output.m
@@ -0,0 +1,9 @@
+function [] = pretty_output(X)
+
+n = length(X)/6;
+fprintf('Node_# trans_x     trans_y     trans_z     rot_x       rot_y        rot_z\n')
+for i = 1:n
+    d = full(X(6*i-5:6*i));
+    fprintf('%-6i %+5.4e %+5.4e %+5.4e %+5.4e %+5.4e %+5.4e\n', i, d)
+end
+
